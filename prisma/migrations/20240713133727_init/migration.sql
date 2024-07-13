4a463d200bf18f3a26cedb2d049504169ff731d5
Tarun Lalwani <tarunlalwani2002@gmail.com>
2024-07-13 19:10:53 +0530

    Prisma initialized and database connected

---
+++ prisma/migrations/20240713133727_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Referral` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `referrerName` VARCHAR(191) NOT NULL,
+    `referrerEmail` VARCHAR(191) NOT NULL,
+    `friendName` VARCHAR(191) NOT NULL,
+    `friendEmail` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
